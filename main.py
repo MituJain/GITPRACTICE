34afc04746c4035f9df363ee1f415f6e816360a3
Nihar Jain <niharjainrockstar@gmail.com>
2024-05-14 07:39:56 +0530

    updated

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ print("I want to become rich.")
 print("It is the 1st line of code.")
 print("It is the 2nd line of code.")
 print("It is the 4rd line of code.")
+print("It is the 1rd line of code.")
+print("It is the 2rd line of code.")
+print("It is the 3rd line of code.")
 
 print("end")
 
