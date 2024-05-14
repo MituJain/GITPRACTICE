faa1654950a4afeba31ad004296be3b13558f4b5
Nihar Jain <niharjainrockstar@gmail.com>
2024-05-14 07:40:26 +0530

    added 8line

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ print("I want to become rich.")
 print("It is the 1st line of code.")
 print("It is the 2nd line of code.")
 print("It is the 3rd line of code.")
+print("It is the 8rd line of code.")
 
 print("end")
 
