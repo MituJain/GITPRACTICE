726415a3213edb3efca6f5f49e93d49b7860ace7
Nihar Jain <niharjainrockstar@gmail.com>
2024-05-14 07:33:42 +0530

    updated line 3 to 4

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ func()
 print("I want to become rich.")
 print("It is the 1st line of code.")
 print("It is the 2nd line of code.")
-print("It is the 3rd line of code.")
+print("It is the 4rd line of code.")
 
 print("end")
 
